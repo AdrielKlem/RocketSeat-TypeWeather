20df5c269d5763ecdd9b1f8228da0e6568256876
Adriel Klem <adrielklem16@gmail.com>
2023-10-24 20:47:07 -0300

    add Type

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -9,6 +9,9 @@ interface GetWeatherByCityProps {
   longitude: number;
 }
 
+type WeatherIconsKeysProps = "Clouds" | "Rain" | "Clear" | "Snow"
+
+
 export interface WeatherAPIResponseProps {
   list: {
     pop: number
@@ -24,7 +27,7 @@ export interface WeatherAPIResponseProps {
       speed: number
     }
     weather: {
-      main: "Clouds" | "Rain" | "Clear" | "Snow"
+      main: WeatherIconsKeysProps
       description: string
     }[]
   }[]
